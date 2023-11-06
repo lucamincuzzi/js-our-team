302be5c1e6a2cd0de2009fb0643af47bfebb7fff
lucamincuzzi <luca.mincuzzi@outlook.it>
2023-11-06 17:16:32 +0100

    add: teamMembers array

---
+++ js/script.js
@@ -0,0 +1,33 @@
+const teamMembers = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        photo: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        photo: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        photo: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manger",
+        photo: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        photo: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Desiger",
+        photo: "barabara-ramos-graphic-designer.jpg"
+    }
+]
+console.log(teamMembers);
